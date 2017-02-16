1c2be00ac1b93c9d7cfc2c83f8c5efe804075cd1
Hasan Pekdemir <hpekdemir.smart@googlemail.com>
2017-02-16 19:05:43 +0100

    Fix comments

--- null.go
+++ null.go
@@ -10,32 +10,32 @@ import (
 // Handler is an example handler storing data nowhere.
 type Handler struct{}
 
-// NewHandler creates a new NullHandler.
+// NewHandler creates a new Handler.
 func NewHandler() *Handler {
 	return &Handler{}
 }
 
-// Insert inserts new items in memory
+// Insert doesn't insert new items
 func (n *Handler) Insert(ctx context.Context, items []*resource.Item) (err error) {
 	return nil
 }
 
-// Update replace an item by a new one in memory
+// Update doesn't replace anything
 func (n *Handler) Update(ctx context.Context, item *resource.Item, original *resource.Item) (err error) {
 	return nil
 }
 
-// Delete deletes an item from memory
+// Delete deletes nothing
 func (n *Handler) Delete(ctx context.Context, item *resource.Item) (err error) {
 	return nil
 }
 
-// Clear clears all items from the memory store matching the lookup
+// Clear clears all items which didn't exist in the first place.
 func (n *Handler) Clear(ctx context.Context, lookup *resource.Lookup) (total int, err error) {
 	return 0, nil
 }
 
-// Find items from memory matching the provided lookup
+// Find items out of nowhere matching the provided lookup
 func (n *Handler) Find(ctx context.Context, lookup *resource.Lookup, offset, limit int) (list *resource.ItemList, err error) {
 	return &resource.ItemList{}, nil
 }
